247e0e5b40dedeb64ed8112e9b156159dd80cc75
Omkar Pote <omkarpote28@gmail.com>
2024-08-06 15:47:12 +0530

    ros2_publisher.py

--- my_py_package/ros2_publisher.py
+++ my_py_package/ros2_publisher.py
@@ -11,8 +11,8 @@ class RobotNewsStationNode(Node):
         
         self.robot_name=" Tarse "
         self._publisher_= self.create_publisher(String, "robot_news",10) #creating publisher
-        self.timer_ = self.create_timer(0.5, self.publish_msg) 
-        self.get_logger().info("Robot News Station has been started")
+        self.timer_ = self.create_timer(0.5, self.publish_msg) #it will call the publish_msg function after every 0.5sec
+        self.get_logger().info("Robot News Station has been started") #it will print the info just like the normal print statement
     
     def publish_msg(self): # msg to be published on the publisher
         msg = String()
@@ -20,10 +20,10 @@ class RobotNewsStationNode(Node):
         self._publisher_.publish(msg) 
 
 def main(args=None):
-    rclpy.init(args=args)
+    rclpy.init(args=args) #it is used to initialize the node
     node = RobotNewsStationNode()
     rclpy.spin(node)
-    rclpy.shutdown()
+    rclpy.shutdown() #it is use to shutdown the node
 
 if __name__ == "__main__":
     main()    
